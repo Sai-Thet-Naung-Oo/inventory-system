b6e62c7ea7296891f4eb133671421e5aebae7cb1
Sai Thet Naung Oo <pyupyu140@gmail.com>
2021-12-04 21:43:40 +0630

    perfect

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 	session_start();
 	// Redirect the user to login page if he is not logged in.
-	// if(!isset($_SESSION['loggedIn'])){
-	// 	header('Location: login.php');
-	// 	exit();
-	// }
+	if(!isset($_SESSION['loggedIn'])){
+		header('Location: login.php');
+		exit();
+	}
 	
 	require_once('inc/config/constants.php');
 	require_once('inc/config/db.php');
