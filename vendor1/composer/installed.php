04937a80da840a8e1590cd5e2fa20bfe3fcabdcc
Sai Thet Naung Oo <pyupyu140@gmail.com>
2021-12-04 21:11:24 +0630

    perfect

---
+++ vendor1/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '938f4645eb0dd36463d4e083608e37969105d596',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '938f4645eb0dd36463d4e083608e37969105d596',
+            'dev_requirement' => false,
+        ),
+    ),
+);
